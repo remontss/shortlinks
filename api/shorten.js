e06c25b0d59c958b8bba513b5b7861b10b74b149
Remontada <143904247+remontss@users.noreply.github.com>
2025-07-01 20:41:31 +0700

    Update shorten.js

--- api/shorten.js
+++ api/shorten.js
@@ -6,8 +6,8 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 
   if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
+  res.status(200).end();
+  return;
   }
 
   if (req.method !== "POST") {

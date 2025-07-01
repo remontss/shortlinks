99a687e98d7c19c75046f30f7f74a24c1947681d
Remontada <143904247+remontss@users.noreply.github.com>
2025-07-01 21:08:01 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,14 @@
 import { getDB } from '../utils/db.js';
 
 export default async function handler(req, res) {
-  const code = req.url.split('/').pop();
+  // Ambil path code secara aman
+  const code = req.query.code || req.url.replace('/', '').split('?')[0];
+
+  if (!code) {
+    res.statusCode = 400;
+    res.end('Code missing');
+    return;
+  }
 
   const db = await getDB();
   const row = await db.get('SELECT url FROM links WHERE code = ?', [code]);

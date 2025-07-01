00455d3a394d3a81cce6c882e399ab1ad610eaec
Remontada <143904247+remontss@users.noreply.github.com>
2025-07-01 20:18:15 +0700

    Update shorten.js

--- api/shorten.js
+++ api/shorten.js
@@ -2,8 +2,8 @@ import { getDB } from '../utils/db.js';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
